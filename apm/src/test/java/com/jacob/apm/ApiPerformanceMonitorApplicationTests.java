13fc7435e5005f969a8e43dd879751afbdb7103c
Jacob Jose <jjose14@asu.edu>
2023-10-01 20:21:28 -0700

    20231001_2020 1% Added template downloaded from start.spring.io.

---
+++ apm/src/test/java/com/jacob/apm/ApiPerformanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.apm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPerformanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
